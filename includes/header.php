6564d6e1528c81b461c9fe78342c5894df19c397
Gleidson <bob_glei@hotmail.com>
2025-05-15 19:05:21 -0300

    15-05-24

--- includes/header.php
+++ includes/header.php
@@ -2,10 +2,12 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <title>Caderno Agroecológico & Solidário</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
+    <link rel="icon" type="image/png" href="../images/favicon.png">   
 </head>
 <body>
 <?php
--- index.php
+++ index.php
@@ -107,6 +107,6 @@ if ($id_agricultor) {
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="js/script.js"></script>
+
 </body>
 </html>
